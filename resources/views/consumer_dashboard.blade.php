6569adb63098871cfee0cbbc0043d3150a9939a3
John Olipani <ampolanch@gmail.com>
2026-01-21 21:20:07 +0800

    Add favicon using mylogo.png

--- resources/views/consumer_dashboard.blade.php
+++ resources/views/consumer_dashboard.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="/images/mylogo.png">
     <title>Document</title>
     <link rel="stylesheet" href="/build/assets/consumer_style-5HLwZ0-W.css">
 </head>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="/images/mylogo.png">
     <title>Admin</title>
     <link rel="stylesheet" href="/build/assets/admin_style-EfzTKaR2.css">
 </head>
